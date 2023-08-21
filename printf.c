f60b30ef53a4eefc67554269fcb0b0b4d2fba77f
nour7mohamed <norhanhassan107@gmail.com>
2023-08-22 02:48:27 +0300

    task 0, 1

--- printf.c
+++ printf.c
@@ -10,37 +10,40 @@ int _printf(const char *format, ...)
 	unsigned int nm, string_count;
 	va_list args;
 
-	if (!format || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-
+	if ((format[0] == '%' && format[1] == '\0')
+	|| (format[0] == '%' && format[1] == ' ') || (format == NULL))
+	return (-1);
 	va_start(args, format);
-
 	for (nm = 0; format[nm] != '\0'; nm++)
 	{
-		if (format[nm] == '%' && format[nm  + 1] == 'c')
+		if (format[nm] == '%' && format[nm + 1] == 'c')
 		{
 			_XcHar(va_arg(args, int));
 			nm++;
 		}
 		else if (format[nm] == '%' && format[nm + 1] == 's')
 		{
-		string_count = makes(va_arg(args, char*));
-		nm++;
-		count += (string_count - 1);
+			string_count = makes(va_arg(args, char *));
+			nm++;
+			count += string_count;
 		}
 		else if (format[nm] == '%' && format[nm + 1] == '%')
 		{
-		_XcHar('%');
-		nm++;
+			_XcHar('%');
+			nm++;
+		}
+		else if (format[nm] == '%' && (format[nm + 1] == 'd'
+					|| format[nm + 1] == 'i'))
+		{
+			int number = va_arg(args, int);
+
+			inthandler(number);
+			nm++;
 		}
 		else
-		{
-		_XcHar(format[nm]);
-		}
-	count += 1;
+			_XcHar(format[nm]);
+		count++;
 	}
-
 	va_end(args);
-
 	return (count);
 }
