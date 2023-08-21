d86c7765205c188e81877fedfff4a419b96b913a
Mohanned-sameh <mohanned2678@gmail.com>
2023-08-21 05:37:41 +0300

    task 1

--- printf.c
+++ printf.c
@@ -10,32 +10,69 @@ int _printf(const char *format, ...)
 	unsigned int nm, string_count;
 	va_list args;
 
-	if (!format || (format[0] == '%' && format[1] == '\0'))
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
 	va_start(args, format);
 
 	for (nm = 0; format[nm] != '\0'; nm++)
 	{
-		if (format[nm] == '%' && format[nm  + 1] == 'c')
+
+		if (format[nm] == '%' && format[nm + 1] == 'c')
 		{
 			_XcHar(va_arg(args, int));
 			nm++;
 		}
 		else if (format[nm] == '%' && format[nm + 1] == 's')
 		{
-		string_count = makes(va_arg(args, char*));
-		nm++;
-		count += (string_count - 1);
+			char *string = va_arg(args, char *);
+			string_count = makes(string);
+			nm++;
+			count += string_count;
 		}
 		else if (format[nm] == '%' && format[nm + 1] == '%')
 		{
-		_XcHar('%');
-		nm++;
+			_XcHar('%');
+			nm++;
+			count++;
+		}
+		else if (format[nm] == '%' && (format[nm + 1] == 'd' || format[nm + 1] == 'i'))
+		{
+			int number = va_arg(args, int);
+			int digit, temp, divider, i, j;
+
+			if (number < 0)
+			{
+				_XcHar('-');
+				count++;
+				number = -number;
+			}
+			digit = 0;
+			temp = number;
+			while (temp != 0)
+			{
+				temp /= 10;
+				digit++;
+			}
+			temp = number;
+			divider = 1;
+
+			for (i = 1; i < digit; i++)
+				divider *= 10;
+			for (j = 0; j < digit; j++)
+			{
+				_XcHar((temp / divider) + '0');
+				temp %= divider;
+				divider /= 10;
+				count++;
+			}
+			nm++;
 		}
 		else
+		{
 			_XcHar(format[nm]);
-	count += 1;
+			count++;
+		}
 	}
 
 	va_end(args);
